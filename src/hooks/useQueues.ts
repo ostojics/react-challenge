7d7d79fa21849976d8328e42ee04fb8cf52e2e41
ostojics <ostojic.dev@gmail.com>
2023-02-18 20:19:43 +0100

    feat: add new queue item implementation

--- src/hooks/useQueues.ts
+++ src/hooks/useQueues.ts
@@ -4,30 +4,59 @@ import { useState } from 'react'
 export const useQueues = (): [IQueue[], (value: number) => void] => {
   const [queues, setQueues] = useState<IQueue[]>([
     { id: 1, itemsAmount: 0, items: [] },
-    { id: 2, itemsAmount: 1, items: [{ id: 7, value: 5 }] },
+    { id: 2, itemsAmount: 5, items: [{ id: 7, value: 5 }] },
     {
       id: 3,
-      itemsAmount: 2,
+      itemsAmount: 13,
       items: [
         { id: 8, value: 3 },
         { id: 9, value: 10 }
       ]
     },
     { id: 4, itemsAmount: 0, items: [] },
-    { id: 5, itemsAmount: 1, items: [{ id: 6, value: 10 }] }
+    { id: 5, itemsAmount: 10, items: [{ id: 6, value: 10 }] }
   ])
 
   const addToQueues = (value: number) => {
-    let isAdded = false
+    const zeroItemsQueue = queues.find((q) => q.itemsAmount < 1)
+
+    if (zeroItemsQueue) {
+      const newQueues = queues.map((queue) => {
+        if (queue.id === zeroItemsQueue.id) {
+          return {
+            ...queue,
+            itemsAmount: queue.itemsAmount + value,
+            items: [...queue.items, { id: Math.random(), value }]
+          }
+        } else {
+          return queue
+        }
+      })
+
+      setQueues(newQueues)
+      return
+    }
+
+    let lowestItemsAmount: number
+    let lowestItemsId: number
+
+    queues.forEach((queue) => {
+      if (!lowestItemsAmount || queue.itemsAmount < lowestItemsAmount) {
+        lowestItemsAmount = queue.itemsAmount
+        lowestItemsId = queue.id
+      }
+    })
 
     const newQueues = queues.map((queue) => {
-      if (queue.itemsAmount < 1 && !isAdded) {
-        queue.items.push({ id: Math.random(), value })
-        queue.itemsAmount = queue.itemsAmount + 1
-        isAdded = true
+      if (queue.id === lowestItemsId) {
+        return {
+          ...queue,
+          itemsAmount: queue.itemsAmount + value,
+          items: [...queue.items, { id: Math.random(), value }]
+        }
+      } else {
+        return queue
       }
-
-      return queue
     })
 
     setQueues(newQueues)
