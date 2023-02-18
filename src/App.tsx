7234c97cbac081233fe3f850bc6d4f1bcd37eb4b
ostojics <ostojic.dev@gmail.com>
2023-02-18 19:35:09 +0100

    feat: add basic algo for checkout

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import Queue from 'components/Queue/Queue'
 import { useQueues } from 'hooks/useQueues'
 
 const App = () => {
-  const [queues, setQueues] = useQueues()
+  const [queues, addToQueues] = useQueues()
+
+  const handleSubmit = (value: number) => {
+    addToQueues(value)
+  }
 
   return (
     <div>
-      <Header />
+      <Header handleSubmit={handleSubmit} />
       <div className="mx-auto mt-[60px] grid w-[720px] grid-cols-5 gap-3">
-        {queues.map(({ id }) => (
+        {queues.map(({ id, items }) => (
           <div key={id} className="text-center">
-            <Queue />
+            <Queue items={items} />
           </div>
         ))}
       </div>
--- src/components/Queue/Queue.tsx
+++ src/components/Queue/Queue.tsx
@@ -1,8 +1,21 @@
-const Queue = () => {
+import { IQueueItem } from 'common/interfaces/Que'
+
+interface IQueueProps {
+  items: IQueueItem[]
+}
+
+const Queue = ({ items }: IQueueProps) => {
   return (
     <div>
       <div className="h-[100px] rounded-md border-2 border-black" />
-      Queue items
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className="mx-auto my-3 flex h-[60px] w-[60px] items-center justify-center rounded-full border-2 border-black text-center "
+        >
+          {item.value}
+        </div>
+      ))}
     </div>
   )
 }
